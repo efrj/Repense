ea1ef9d027c45eabb16f110356d74b22a54eb63a
efrj <efrj@hotmail.com>
2016-02-03 00:00:14 -0200

    Create student model, course model, classroom model

---
+++ spec/support/factories/classrooms.rb
@@ -0,0 +1,9 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :classroom do
+    student nil
+    course nil
+    entry_at "2016-02-02 21:53:13"
+  end
+end
